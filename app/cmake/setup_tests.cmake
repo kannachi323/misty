1649b9597340195f54748f7c4df0e7e066135a95
kannachi323 <mattdev727@gmail.com>
2026-01-30 23:54:18 -0800

    fixing vendors and refactoring folder names

---
+++ app/cmake/setup_tests.cmake
@@ -0,0 +1,12 @@
+# cmake/setup_tests.cmake
+
+file(GLOB_RECURSE TEST_SRCS
+    "src/tests/*.cpp"
+    "src/tests/*.h"
+)
+# tests
+add_executable(minidfs_tests ${TEST_SRCS})
+target_link_libraries(minidfs_tests PRIVATE
+    minidfs
+    gtest_main
+)
